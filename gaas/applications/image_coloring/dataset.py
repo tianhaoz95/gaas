554a06f03a42e46690a2d04bd2376d8e94a540da
Tianhao Zhou <tianhaoz@umich.edu>
2020-12-27 16:46:34 -0800

    chore: deprecate datase fetcher

--- gaas/applications/image_coloring/dataset.py
+++ gaas/applications/image_coloring/dataset.py
@@ -30,31 +30,10 @@ class AnimeSketchColorizationDatasetGenerator:
         self._extract_location = get_extract_location(self._data_dir,
                                                       self._dataset_id)
         self._logger = logging.getLogger()
-        # self._maybe_fetch_kaggle_dataset()
         self._kaggle_credential = get_kaggle_credential()
         maybe_fetch_kaggle_dataset(self._data_dir, self._kaggle_id,
                                    self._dataset_id, self._kaggle_credential)
         self._maybe_extract_kaggle_dataset()
-
-    def _maybe_fetch_kaggle_dataset(self) -> None:
-        proc = Popen(self._fetch_kaggle_dataset_args,
-                     cwd=self._data_dir,
-                     stdin=PIPE,
-                     stdout=PIPE,
-                     stderr=PIPE)
-        _, stderr = proc.communicate()
-        if proc.returncode != 0:
-            self._logger.error(
-                'Fetch dataset from Kaggle failed with return code {ret}'.
-                format(ret=proc.returncode))
-            self._logger.error('Error message: {msg}'.format(msg=stderr))
-            if 'KAGGLE_USERNAME' not in os.environ:
-                self._logger.warn(
-                    'KAGGLE_USERNAME not found in the environment.')
-            if 'KAGGLE_KEY' not in os.environ:
-                self._logger.warn('KAGGLE_KEY not found in the environment.')
-            raise RuntimeError('Fetch Kaggle dataset failed.')
-        self._logger.info('Fetch Kaggle dataset done.')
 
     def _maybe_extract_kaggle_dataset(self) -> None:
         if os.path.exists(self._extract_location):
